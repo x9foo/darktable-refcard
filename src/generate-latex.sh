a43209d4b82ad0a5e765727f09867f330dc7bd72
x9foo <x9foo@richtmarke.de>
2015-11-30 11:48:52 +0100

    Add version support.
    Add a version number to generate-latex.sh. Use it in generated
    filenames and in the document footer.

--- src/generate-latex.sh
+++ src/generate-latex.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+version='0.2pre'
 formats='a4paper letter'
 languages='english german french'
 colors='dark bright'
@@ -9,13 +10,14 @@ colors='dark bright'
 for format in $formats; do
     for language in $languages; do
         for color in $colors; do
-            base="darktable-refcard-$format-$language-$color"
+            base="darktable-refcard-$format-$language-$color-$version"
             texfile="$base.tex"
             echo "Generating file $texfile"
             cat << EOF > "$texfile"
 \def\ArgLang{$language}
 \def\ArgFormat{$format}
 \def\ArgColor{$color}
+\def\ArgVersion{$version}
 \input main
 EOF
         done
